462a4d5906be5709bf8aa45a27e7b1cb13a31de2
Conor <ConorP95@hotmail.co.uk>
2018-05-31 21:38:38 +0100

    Added in default local multiplayer camera

---
+++ BlockOut/Source/BlockOut/BP_Camera.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BP_Camera.generated.h"
+
+UCLASS()
+class BLOCKOUT_API ABP_Camera : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABP_Camera();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	
+	
+};
